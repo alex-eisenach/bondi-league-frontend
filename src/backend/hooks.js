c643353633a8f06dadf04c82871c360e3cbc71b9
Alex Eisenach <alex.eisenach303@gmail.com>
2025-12-29 00:43:34 -0700

    make URL automatic so no need to switch when pushing to Render

--- src/backend/hooks.js
+++ src/backend/hooks.js
@@ -1,5 +1,6 @@
-const API_URL = "http://localhost:5038";
-//const API_URL = "https://golf-league-scoretracker-backend.onrender.com";
+const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+  ? "http://localhost:5038"
+  : "https://golf-league-scoretracker-backend.onrender.com";
 
 export async function postNewGolfer(_data) {
   const urlPost = API_URL + '/posts/newGolfer';
